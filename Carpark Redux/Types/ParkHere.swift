baa9e0c7845963cfd8eb584ea0fdbb8fd2de6792
Mack Slevin <mack.slevin@gmail.com>
2024-04-02 18:14:25 -0700

    Add app intent functionality to save new spot to SwiftData

--- Carpark Redux/Types/ParkHere.swift
+++ Carpark Redux/Types/ParkHere.swift
@@ -7,21 +7,59 @@
 
 import Foundation
 import AppIntents
+import SwiftData
+
+
+
+
 
 struct ParkHere: AppIntent {
     
     
     static var title: LocalizedStringResource = "Park Here"
     
-    func perform() async throws -> some IntentResult {
+    
+    
+    enum ParkHereError: Swift.Error, CustomLocalizedStringResourceConvertible {
+        case noLocation
         
+        var localizedStringResource: LocalizedStringResource {
+            switch self {
+                case .noLocation:
+                    return "Location unavailable"
+            }
+        }
+    }
+    
+    
+    @MainActor
+    func perform() async throws -> some IntentResult {
+        // Get user location
         let locationModel = LocationModel()
         locationModel.requestAuthorization()
         
         if let location = locationModel.locationManager.location {
             print("^^ location \(location)")
+            
+            // Create model container to interact with SwiftData database
+            var sharedModelContainer: ModelContainer = {
+                let schema = Schema([
+                    ParkingSpot.self,
+                ])
+                let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false, cloudKitDatabase: .private("iCloud.com.johnslevin.Carpark"))
+
+                do {
+                    return try ModelContainer(for: schema, configurations: [modelConfiguration])
+                } catch {
+                    fatalError("Could not create ModelContainer: \(error)")
+                }
+            }()
+            
+            let newSpot = ParkingSpot(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
+            sharedModelContainer.mainContext.insert(newSpot)
         } else {
             print("^^ no location")
+            throw ParkHereError.noLocation
         }
         
         return .result()
