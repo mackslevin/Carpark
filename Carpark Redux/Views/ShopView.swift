9b8794d1d811930a4d983840fcc3e7a2b627a800
Mack Slevin <mack.slevin@gmail.com>
2024-10-31 15:24:39 -0700

    Switch out StoreView for custom UI

--- Carpark Redux/Views/ShopView.swift
+++ Carpark Redux/Views/ShopView.swift
@@ -15,14 +15,72 @@ struct ShopView: View {
     @State private var purchaseError: IAPError? = nil
     
     var body: some View {
-        StoreView(products: products)
-            .task {
-                products = await skClient.products
+            ScrollView {
                 
-                for product in products {
-                    let isPurchased = try! await skClient.isPurchased(product.id)
-                    print("^^ Has purchased \(product.displayName)? \(isPurchased)")
+                
+                
+                VStack(alignment: .leading, spacing: 12) {
+                    VStack {
+                        HStack {
+                            Image("mack")
+                                .resizable().scaledToFill()
+                                .frame(maxWidth: 60, maxHeight: 60)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                            
+                            Text("Hi, I'm Mack!")
+                                .font(.title)
+                                .fontWeight(.semibold)
+                            Spacer()
+                        }
+                        
+
+                        Text("I'm an independant software developer in Los Angeles, California. I first made this app back in 2015 when I was living next to Dodger Stadium and often had to get creative with my parking on game nights. Since then I've continued to refine the app and add new features to make it even better. If you enjoy Carpark, please consider throwing a tip my way! It'll help fund future development, in addition to earning you my immense grattitude 😊")
+                            .italic()
+                    }
+                    .padding()
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity)
+                    .background {
+                        Rectangle().foregroundStyle(Color.accentColor.gradient)
+                    }
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                    .shadow(radius: 2, x: 0, y: 1)
+                    
+                    ForEach(products) { product in
+                        HStack {
+                            ProductView(product)
+                                .productViewStyle(.compact)
+                            Spacer()
+                        }
+                        .padding()
+                        .background {
+                            RoundedRectangle(cornerRadius: 20)
+                                .foregroundStyle(.primary).colorInvert()
+                                .shadow(radius: 2, x: 0, y: 1)
+                        }
+                    }
+                    
+                    Spacer()
+                    
+                    Button("Restore Purchase") {
+                        Task {
+                            do {
+                                try await AppStore.sync()
+                            } catch {
+                                purchaseError = .system(error)
+                            }
+                        }
+                    }
+                    .tint(.secondary)
+                    .frame(maxWidth: .infinity)
                 }
+                .padding()
+                
+                
+            }
+            .navigationTitle("Tip Jar")
+            .task {
+                products = await skClient.products.sorted(by: {$0.price < $1.price})
             }
             .storeButton(.visible, for: .restorePurchases)
             .onInAppPurchaseCompletion { product, result in
@@ -44,7 +102,7 @@ struct ShopView: View {
             } message: {
                 Text(purchaseError?.localizedDescription ?? "")
             }
-    }
+        }
 }
 
 #Preview {
